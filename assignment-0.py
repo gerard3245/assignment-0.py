50badcc052fe07fbe1387b7117871245345f7b4a
gerard3245 <105588631+gerard3245@users.noreply.github.com>
2022-05-16 10:28:02 +0800

    Add files via upload

---
+++ assignment-0.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+'''Assignment 0
+
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+def three_number_average(x, y, z):
+    '''Item 1.
+    Three Number Average. 3 points.
+
+    Returns the average of three numbers.
+
+    Parameters
+    ----------
+    x: int
+        the first number
+    y: int
+        the second number
+    z: int
+        the third number
+
+    Returns
+    -------
+    float
+        the average of x, y, and z
+    '''
+    # Write your code below this line 
+    n1 = int(input("enter first value:"))  
+    n2 = int(input("enter second value:"))
+    n3 = int(input("enter third value:"))
+    avaln = (n1+n2+n3)/3
+    print("the average is:",avaln)
+    
+
+
+# In[2]:
+
+
+n1 = int(input("enter first value:"))
+
+
+# In[5]:
+
+
+n1 = int(input("enter first value:"))  
+n2 = int(input("enter second value:"))
+n3 = int(input("enter third value:"))
+avaln = (n1+n2+n3)/3
+
+
+# In[6]:
+
+
+n1 = int(input("enter first value:"))
+
+
+# In[7]:
+
+
+n1 = int(input("enter first value:"))  
+n2 = int(input("enter second value:"))
+n3 = int(input("enter third value:"))
+avaln = (n1+n2+n3)/3
+print("the average is:",avaln)
+
+
+# In[ ]:
+
+
+n1 = int(input("enter first value:"))  
+n2 = int(input("enter second value:"))
+n3 = int(input("enter third value:"))
+avaln = (n1+n2+n3)/3
+print("the average is:",avaln)
+
